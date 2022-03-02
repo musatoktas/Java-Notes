97a49d4fcfa61f7b01a78f43e29b92a9c47914a7
Musa Toktas <musatoktas@users.noreply.github.com>
2022-03-02 23:29:47 +0300

    Create alertin.java

---
+++ alertin.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class IlkDialog {
+    
+        
+    public static void main(String[] args){
+        
+        JOptionPane.showMessageDialog(null,"İlk Java Dialog");
+    }
+}
